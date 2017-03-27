a79c2e8a9b5bf7c6e5154563733216a47ac7e9d3
Alfredo Oliveira <alfredo.coj@gmail.com>
2017-03-27 10:39:38 -0300

    criado classe EntityBase

---
+++ src/main/java/com/uema/admin/entities/EntityBase.java
@@ -0,0 +1,52 @@
+package com.uema.admin.entities;
+
+import com.uema.log.Auditavel;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by alfredo on 27/03/17.
+ */
+abstract public class EntityBase extends Auditavel{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    protected Integer id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "data_alteracao", insertable = false)
+    protected Date dataAlteracao;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "data_criacao", updatable = false)
+    protected Date dataCriacao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDataAlteracao() {
+        return dataAlteracao;
+    }
+
+    public void setDataAlteracao(Date dataAlteracao) {
+        this.dataAlteracao = dataAlteracao;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+
+}
+
